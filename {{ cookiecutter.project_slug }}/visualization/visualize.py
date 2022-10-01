ffb1bc8f56cac7888bec16f9da2ac9ccc04ecac5
oecorrechag <oecorrechag@unal.edu.co>
2022-09-30 22:31:35 -0500

    se cambio el grafico plotly a sns

--- {{ cookiecutter.project_slug }}/visualization/visualize.py
+++ {{ cookiecutter.project_slug }}/visualization/visualize.py
@@ -1,9 +1,10 @@
-import re
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-import plotly.express as px
 
 def scatter_plot_x(data: pd.DataFrame, x, y):
-    fig = px.scatter(data, x=x, y=y)
-    return fig
+    sns.lineplot(
+        data=data,
+        x=x,
+        y=y
+    )
